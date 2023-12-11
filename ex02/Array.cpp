6e473542aafe256a1e0eecbffed2a85365104b30
Hassan Lakhal <hlakhal-@e1r5p10.1337.ma>
2023-12-11 18:41:11 +0100

    fixed error

--- ex02/Array.cpp
+++ ex02/Array.cpp
@@ -6,33 +6,33 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 18:19:41 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/12/09 15:10:50 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/12/10 17:44:31 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"Array.hpp"
-int main() {
-    try 
-    {
-        Array<int> arr1;
-        std::cout << "arr1 size: " << arr1.size() << std::endl;
+// #include"Array.hpp"
+// int main() {
+//     try 
+//     {
+//         Array<int> arr1;
+//         std::cout << "arr1 size: " << arr1.size() << std::endl;
 
-        Array<double> arr2(5);
-        std::cout << "arr2 size: " << arr2.size() << std::endl;
+//         Array<double> arr2(5);
+//         std::cout << "arr2 size: " << arr2.size() << std::endl;
 
-        Array<int> arr3 = arr1;
-        std::cout << "arr3 size: " << arr3.size() << std::endl;
+//         Array<int> arr3 = arr1;
+//         std::cout << "arr3 size: " << arr3.size() << std::endl;
 
-        arr2[2] = 42.42;
-        arr3 = arr2;
-        std::cout << "arr3 size: " << arr3.size() << std::endl;
-        std::cout << "arr2[2]: " << arr3[2] << std::endl;
-        std::cout << "arr2[2]: " << arr2[2] << std::endl;
+//         arr2[2] = 42.42;
+//         arr3 = arr2;
+//         std::cout << "arr3 size: " << arr3.size() << std::endl;
+//         std::cout << "arr2[2]: " << arr3[2] << std::endl;
+//         std::cout << "arr2[2]: " << arr2[2] << std::endl;
         
-    }
-     catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+//     }
+//      catch (const std::exception& e) {
+//         std::cerr << "Exception: " << e.what() << std::endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 18:08:50 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/12/09 15:11:28 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/12/11 18:36:20 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ class Array
                 throw std::runtime_error("out of range !!");
             return elements[n];
         }
-
         T* getElement() const
         {
             return this->elements; 
@@ -47,19 +46,17 @@ class Array
         {
             delete [] elements;
         }
-        template<typename U>
-        Array& operator =(const Array<U>& other)
+        Array& operator =(const Array& other)
         {
-            if(this != static_cast<const void*>(&other))
+            if(this != &other)
             {
                 delete [] elements;
-                this->elements = new T[other.size()];
-                for (unsigned int i = 0; i < other.size(); i++)
+                this->elements = new T[other.numSize];
+                for (unsigned int i = 0; i < other.numSize; i++)
                 {
-                   this->elements[i] = other.getElement()[i];
+                   this->elements[i] = other.elements[i];
                 }
                 this->numSize = other.size();
-                 
             }
             return *this;
         }
